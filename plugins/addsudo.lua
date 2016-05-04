ab96412fea72388f6213b695508342dfdf622c03
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-05 01:13:59 +0430

    Update addsudo.lua

--- plugins/addsudo.lua
+++ plugins/addsudo.lua
@@ -8,34 +8,34 @@ end
 local function run(msg, matches)
  if matches[1] == 'sudo1' then
         chat = 'chat#'..msg.to.id
-        user1 = 'user#'..157059515
+        user1 = 'user#'..147237496
         chat_add_user(chat, user1, callback, false)
-	return "SoLiD Is Coming :/"
+	return "مدیر ربات با موفقیت وارد گروه شد :/"
       end
   if matches[1] == 'sudo2' then
         chat = 'chat#'..msg.to.id
-        user2 = 'user#'..140529465
+        user2 = 'user#'..184413821
         chat_add_user(chat, user2, callback, false)
-	return "MrHalix Is Coming :D"
+	return "مدیر دوم ربات با موفقیت وارد گروه شد :D"
       end
 if matches[1] == 'kosgo' or matches[1] == 'kosgu' then
         chat = 'chat#'..msg.to.id
-        user3 = 'user#'..170324143
+        user3 = 'user#'..170327103
         chat_add_user(chat, user3, callback, false)
-	return "KosGu Is Coming :D"
+	return "درست نمیباشد :D"
       end
  if matches[1] == 'jack' then
         chat = 'chat#'..msg.to.id
-        user4 = 'user#'..119256329
+        user4 = 'user#'..11710471
         chat_add_user(chat, user4, callback, false)
-	return "adding Jack(For test) :D"
+	return "درست نمیباشد) :D"
       end
  end
 
 return {
-  description = "Invite X Y Z C B Robots", 
+  description = "ساپورت تیم", 
   usage = {
-    "/zac : invite x y z c b bots", 
+    "/help دریافت دستورات", 
 	},
   patterns = {
     "^[!/.](sudo1)",
