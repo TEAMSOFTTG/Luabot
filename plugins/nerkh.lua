5682b614bbcfa92ebe68d524930df86cc6505e2c
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-05 17:07:22 +0430

    Added files via upload

---
+++ plugins/nerkh.lua
@@ -0,0 +1,22 @@
+do
+
+function run(msg, matches)
+  return "نــرخـ ساختـ گروهـ🔽\n 15 روز اول رایگان برای تست🔥"
+end
+return {
+  description = "Nerkh Sakht Group", 
+  usage = "!join [invite link]",
+  patterns = {
+    "^/nerkh$",
+    "^!nerkh$",
+    "^nerkh$",
+    "^nerkh$",
+   "^/Nerkh$",
+   "^!Nerkh$",
+   "^Nerkh$",
+   "^نرخ$",
+
+  },
+  run = run
+}
+end
