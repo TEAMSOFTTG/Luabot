7f3e50cdd2b7e613f5daee0ac150cfa5d969ecc2
TEAMSOFTTG <tgteam351@gmail.com>
2016-05-05 20:18:12 +0430

    Update config.lua

--- data/config.lua
+++ data/config.lua
@@ -1,5 +1,5 @@
 do local _ = {
-  about_text = "Tele Beyond V4.5\nAn Administrator The PowerFull Bot Based On TeleSeed And Bilingual Written On Lua\n\nSudo Users :\nDeveloper&Founder : @SoLiD021\nManager&Helper : @MrHalix\n\nTeam Channel :\n@BeyondTeam\n\nSpecial Thx To :\n@idivanmanheb\nBeyond Team\nAnd All My Friends\n",
+  about_text = "Soft TG V1\nAn Administrator The PowerFull Bot Based On TeleSeed",
   disabled_channels = {},
   disabled_plugin_on_chat = {
     ["chat#id108270013"] = {
@@ -59,7 +59,7 @@ do local _ = {
     "onservice",
     "inrealm",
     "inpm",
-    "Help",
+    "help",
     "plugins",
     "joining",
     "addplug",
@@ -100,7 +100,7 @@ do local _ = {
     "ECHO",
     "echo",
     "Calculator",
-    "GET",
+    "get",
     "link",
     "invite2",
     "banhammer",
@@ -117,9 +117,7 @@ do local _ = {
     "antifosh",
     "lock_eng",
     "antisticker",
-    "tosupp",
     "txt2stick",
-    "ingroup2",
     "setstick",
     "welcome",
     "chat",
@@ -132,7 +130,6 @@ do local _ = {
     "share_contact",
     "help_pv",
     "filterword",
-    "apk",
     "time2",
     "weather",
     "instagram",
@@ -145,7 +142,10 @@ do local _ = {
     "lock_chat",
     "lock_emoji",
     "antispam_gp",
-    "antispam_pv"
+    "antispam_pv",
+    "lock_fa",
+    "lock_sticker",
+    "lock_gif"
   },
   help_text = "Commands list :\n!kick [username|id]\nYou can also do it by reply\n!ban [ username|id]\nYou can also do it by reply\n!unban [id]\nYou can also do it by reply\n!who\nMembers list\n!modlist\nModerators list\n!promote [username]\nPromote someone\n!demote [username]\nDemote someone\n!kickme\nWill kick user\n!about\nGroup description\n!setphoto\nSet and locks group photo\n!setname [name]\nSet group name\n!rules\nGroup rules\n!id\nreturn group id or user id\n!help\n!lock [member|name|bots|leave]\9\nLocks [member|name|bots|leaveing] \n!unlock [member|name|bots|leave]\nUnlocks [member|name|bots|leaving]\n!set rules <text>\nSet <text> as rules\n!set about <text>\nSet <text> as about\n!settings\nReturns group settings\n!newlink\ncreate/revoke your group link\n!link\nreturns group link\n!owner\nreturns group owner id\n!setowner [id]\nWill set id as owner\n!setflood [value]\nSet [value] as flood sensitivity\n!stats\nSimple message statistics\n!save [value] <text>\nSave <text> as [value]\n!get [value]\nReturns text of [value]\n!clean [modlist|rules|about]\nWill clear [modlist|rules|about] and set it to nil\n!res [username]\nreturns user id\n\"!res @username\"\n!log\nwill return group logs\n!banlist\nwill return group ban list\n**U can use both \"/\" and \"!\" \n*Only owner and mods can add bots in group\n*Only moderators and owner can use kick,ban,unban,newlink,link,setphoto,setname,lock,unlock,set rules,set about and settings commands\n*Only owner can use res,setowner,promote,demote and log commands\n",
   help_text_realm = "Realm Commands:\n!creategroup [Name]\nCreate a group\n!createrealm [Name]\nCreate a realm\n!setname [Name]\nSet realm name\n!setabout [GroupID] [Text]\nSet a group's about text\n!setrules [GroupID] [Text]\nSet a group's rules\n!lock [GroupID] [setting]\nLock a group's setting\n!unlock [GroupID] [setting]\nUnock a group's setting\n!wholist\nGet a list of members in group/realm\n!who\nGet a file of members in group/realm\n!type\nGet group type\n!kill chat [GroupID]\nKick all memebers and delete group\n!kill realm [RealmID]\nKick all members and delete realm\n!addadmin [id|username]\nPromote an admin by id OR username *Sudo only\n!removeadmin [id|username]\nDemote an admin by id OR username *Sudo only\n!list groups\nGet a list of all groups\n!list realms\nGet a list of all realms\n!log\nGrt a logfile of current group or realm\n!broadcast [text]\n!broadcast Hello !\nSend text to all groups\nOnly sudo users can run this command\n!bc [group_id] [text]\n!bc 123456789 Hello !\nThis command will send text to [group_id]\n**U can use both \"/\" and \"!\" \n*Only admins and sudo can add bots in group\n*Only admins and sudo can use kick,ban,unban,newlink,setphoto,setname,lock,unlock,set rules,set about and settings commands\n*Only admins and sudo can use res, setowner, commands\n",
@@ -153,9 +153,7 @@ do local _ = {
     data = "data/moderation.json"
   },
   sudo_users = {
-    157059515,
-    140529465,
-    103214508
+    147237496
   }
 }
 return _
